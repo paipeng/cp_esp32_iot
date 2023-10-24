72a041e549d3ee633c959a607109aa1c8bb886fa
Pai Peng <sipaipv6@gmail.com>
2023-10-24 21:24:36 +0800

    use const, mac address as udid

--- cp_esp32_iot.ino
+++ cp_esp32_iot.ino
@@ -5,6 +5,9 @@
 #include <PubSubClient.h>
 #include "public.h"
 #include "wifi_const.h"
+#include "mqtt_const.h"
+
+
 
 // WIFI_SSID and WIFI_PASSWD are defined in wifi_const.h
 //#define WIFI_SSID ""
@@ -12,17 +15,19 @@
 
 // MQTT Broker
 
-#define MQTT_BROKER "192.168.1.228"
-#define MQTT_PORT 1883
+//#define MQTT_BROKER "192.168.1.228"
+//#define MQTT_PORT 1883
 
-#define MQTT_USERNAME "admin"
-#define MQTT_PASSWORD "public"
+//#define MQTT_USERNAME "admin"
+//#define MQTT_PASSWORD "public"
 
-#define MQTT_TOPIC_PREFIX "CP_IOT"
-#define MQTT_TOPIC_PING "PING"
-#define MQTT_TOPIC_PONG "PONG"
+//#define MQTT_TOPIC_PREFIX "CP_IOT/"
+//#define MQTT_TOPIC_PING "PING"
+//#define MQTT_TOPIC_PONG "PONG"
 
-#define DEVICE_UUID "420d2b68-6a4a-11ee-8c99-0242ac120002"
+char DEVICE_UDID[18];
+
+
 
 WiFiMulti WiFiMulti;
 
@@ -43,6 +48,11 @@ void wifi_connect(void)
   Serial.println();
   Serial.print("\nDefault ESP32 MAC Address: ");
   Serial.println(WiFi.macAddress());
+  
+  Serial.print("\nDevice UUID: ");
+  strncpy(DEVICE_UDID, WiFi.macAddress().c_str(), 17);
+  Serial.println(DEVICE_UDID);
+  
   Serial.print("\n\nWaiting for WiFi... ");
 
   while(WiFiMulti.run() != WL_CONNECTED) {
@@ -68,13 +78,13 @@ void setup(){
 void mqtt_connect() {
   Serial.println("Connected to the Wi-Fi network");
   //connecting to a mqtt broker
-  mqttClient.setServer(mqtt_broker, mqtt_port);
+  mqttClient.setServer(MQTT_BROKER, MQTT_PORT);
   mqttClient.setCallback(callback);
   while (!mqttClient.connected()) {
-      String client_id = "esp32-client-";
+      String client_id = "cp-esp32-client-";
       client_id += String(WiFi.macAddress());
       Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
-      if (mqttClient.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+      if (mqttClient.connect(client_id.c_str(), MQTT_USERNAME, MQTT_PASSWORD)) {
           Serial.println("Public EMQX MQTT broker connected");
       } else {
           Serial.print("failed with state ");
@@ -83,8 +93,13 @@ void mqtt_connect() {
       }
   }
   // Publish and subscribe
-  mqttClient.publish(publish_topic, "{\"deviceUUID\": \"420d2b68-6a4a-11ee-8c99-0242ac120002\", \"state\": 1}");
-  mqttClient.subscribe(subscribe_topic);
+  String mqttPong = String(MQTT_TOPIC_PREFIX) + String(MQTT_TOPIC_PONG);
+  Serial.print("publish topic: " + mqttPong);
+  mqttClient.publish(mqttPong.c_str(), "{\"udid\": \"420d2b68-6a4a-11ee-8c99-0242ac120002\", \"state\": 1}");
+  String mqttPing = String(MQTT_TOPIC_PREFIX) + String(DEVICE_UDID) + "/" + String(MQTT_TOPIC_PING);
+  
+  Serial.print("\nsubscribe topic: " + mqttPing + "...\n");
+  mqttClient.subscribe(mqttPing.c_str());
 }
 
 void callback(char *topic, byte *payload, unsigned int length) {
---
+++ mqtt_const.h
@@ -0,0 +1,18 @@
+#ifndef _mqtt_const_H
+#define _mqtt_const_H
+
+
+
+#define MQTT_BROKER "192.168.1.228"
+#define MQTT_PORT 1883
+
+#define MQTT_USERNAME "admin"
+#define MQTT_PASSWORD "public"
+
+#define MQTT_TOPIC_PREFIX "CP_IOT/"
+#define MQTT_TOPIC_PING "PING"
+#define MQTT_TOPIC_PONG "PONG"
+
+//#define DEVICE_UUID "420d2b68-6a4a-11ee-8c99-0242ac120002"
+
+#endif //_mqtt_const_H
